9c0155a67d5e407eab2f2478df719c8d819242a0
Grégory <gregory.schoemaecker@outlook.fr>
2023-07-25 20:51:41 +0200

    ExerciceScientifique

---
+++ ExerciceScientifique/Classe/Ihm.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciceScientifique.Classe
+{
+    internal static class Ihm
+    {
+        public static void IHM()
+        {
+            Travailleur travailleur = new Travailleur("Schoemaecker", "Aurélie", "0657575757777"
+                , "lilie@gmail.com", "Mairie", "32 rue des la bastille", "070606461606");
+            Console.WriteLine(travailleur);
+            Console.WriteLine();
+            Scientifique scientifique = new Scientifique("Schoemaecker", "Bastien", "0657575757",
+                "lilie@gmail.com", "Mairie", "32 rue des la bastille", "070606461606", "Chimie", "Théorique");
+            Console.WriteLine(scientifique);
+        }
+        
+    }
+}
