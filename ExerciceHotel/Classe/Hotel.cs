8533017121c6f740c39fb5509d122a7c478f88ec
Grégory Schoemaecker <gregory.schoemaecker@outlook.fr>
2023-07-26 15:10:23 +0200

    ExerciceHotel

---
+++ ExerciceHotel/Classe/Hotel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciceHotel.Classe
+{
+    internal class Hotel
+    {
+        private List<Client> clients = new List<Client>();
+        private List<Chambre> chambres = new List<Chambre>();
+        private Client Client { get; set; } 
+    }
+}
