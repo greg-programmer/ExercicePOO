39a0aa8c86dfa72c74aa287f839fe35174264397
Grégory <gregory.schoemaecker@outlook.fr>
2023-08-12 18:03:33 +0200

    ExerciceVol

--- ExerciceVol/Classes/Ihm.cs
+++ ExerciceVol/Classes/Ihm.cs
@@ -9,14 +9,11 @@ namespace ExerciceVol.Classes
     internal class Ihm
     {
         public void IHM()
-        {
-            List<VolDirect> Directs = new List<VolDirect>();
-            VolDirect DirectA = new VolDirect("Paris", "Marseille", "12", new DateTime());
-            VolDirect DirectB = new VolDirect("Paris", "Toulouse", "17", new DateTime());
-            VolDirect DirectC = new VolDirect("Paris", "Barcelone", "22", new DateTime());
-            Console.WriteLine(DirectA);
-            Console.WriteLine(DirectB);
-            Console.WriteLine(DirectC);
+        {        
+            Vols vols = new Vols();
+            vols.Affiche();
+            Console.WriteLine();
+            vols.Appartient();
         }     
     }
 }
--- ExerciceVol/Classes/Vols.cs
+++ ExerciceVol/Classes/Vols.cs
@@ -9,8 +9,17 @@ namespace ExerciceVol.Classes
 {
     internal class Vols : IVol
     {
-        List<VolDirect> Vol { get; set; } = new List<VolDirect>();
-        List<string> VolNamesArrived { get; set; } = new List<string>();
+       List<VolDirect> Vol { get; set; } = new List<VolDirect>() 
+        {
+           new VolDirect("Paris", "Marseille", "12", new DateTime()),
+           new VolDirect("Marseille", "Barcelone", "22", new DateTime()),
+           new VolDirect("Toulouse", "Barcelone", "22", new DateTime()),
+           new VolDirect("Paris", "Toulouse", "17", new DateTime()),
+           new VolDirect("Paris", "Barcelone", "22", new DateTime()),
+           new VolDirect("Lille", "Barcelone", "22", new DateTime()),
+           new VolDirect("Toulouse", "Paris", "22", new DateTime()),
+        };
+        List<string> VolNamesArrived { get; set; } = new List<string>() { "Paris","Barcelone","Rome","Venise","Toulouse","Lille","Madrid"};
 
         public List<string> Liste_Successeurs(string villeDepart)
         {
@@ -28,10 +37,25 @@ namespace ExerciceVol.Classes
             }
         }
 
-        public bool Appartient()
+        //Écrire une méthode Appartient qui vérifie si une ville appartient au plan du vol que ce soit comme ville d’arrivée ou de départ
+        public void Appartient()
         {
-            throw new NotImplementedException();
+            bool appartient = false;
+           var a = new List<VolDirect>();
+            foreach (var item in VolNamesArrived)
+            {
+               a = Vol.Where(volDirects => volDirects.Start == item || volDirects.Arrived == item).ToList();
+                if(a.Count == 0)
+                {
+                    Console.WriteLine($"La ville de {item} ne fait pas partie du plan de de vol !");
+                }
+                else
+                {
+                    Console.WriteLine($"La ville de {item} fait partie du plan de vol !");
+                }              
+
+           }     
+            
         }
-   
     }
 }
